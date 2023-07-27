f9d088aaf157c8946e2f23ee0306c1d496721f24
tonybuii2003 <longphibui1993@gmail.com>
2023-07-27 10:02:02 +0700

    feat: add completed Starting with Express

--- backend-development-and-apis/boilerplate-express/myApp.js
+++ backend-development-and-apis/boilerplate-express/myApp.js
@@ -1,5 +1,11 @@
 let express = require('express');
 let app = express();
+console.log("Hello World")
+
+
+app.get("/", function (req, res) {
+    res.send("Hello Express");
+});
 
 
 
@@ -32,8 +38,4 @@ let app = express();
 
 
 
-
-
-
-
- module.exports = app;
+module.exports = app;
