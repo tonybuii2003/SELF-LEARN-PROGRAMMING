f0f04b6a96c17b722e01c86a0181a695854a61b0
tonybuii2003 <longphibui1993@gmail.com>
2023-07-29 19:55:37 +0700

    feat: add completed Basic Node and Express

--- backend-development-and-apis/boilerplate-express/myApp.js
+++ backend-development-and-apis/boilerplate-express/myApp.js
@@ -1,13 +1,60 @@
 let express = require('express');
+let bodyParser = require('body-parser');
 let app = express();
-console.log("Hello World")
-
-
-app.get("/", function (req, res) {
-    res.send("Hello Express");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.post("/name", (req, res) => {
+    firstname = req.body.first;
+    lastname = req.body.last;
+    var full_name = "" + firstname + " " + lastname;
+    res.json({ name: full_name });
+});
+app.use(function middleware(req, res, next) {
+    var string = req.method + " " + req.path + " - " + req.ip;
+    console.log(string);
+    next();
+});
+//get current time
+app.get("/now", (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+}, (req, res) => {
+    res.json({ "time": req.time });
+});
+//get the route before echo must use /:word/echo
+app.get("/:word/echo", (req, res) => {
+    res.json({ echo: req.params['word'] });
+});
+app.get("/name", (req, res) => {
+    firstname = req.query.first;
+    lastname = req.query.last;
+    var full_name = "" + firstname + " " + lastname;
+    res.json({ name: full_name });
 });
 
+require('dotenv').config();
+console.log("Hello World")
 
+// app.get("/", function (req, res) {
+//     res.send("Hello Express");
+// });
+
+
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html");
+});
+
+app.use("/public", express.static(__dirname + "/public"))
+
+app.get("/json", (req, res) => {
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        res.json({ "message": "Hello json".toUpperCase() });
+    } else {
+        res.json({ "message": "Hello json" });
+    }
+
+});
 
 
 
